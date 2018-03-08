2c94960d880d398eff9ae48c895987877254a412
Bruno Dutra <brunocodutra@gmail.com>
2018-03-08 21:31:56 +0100

    expose the entire webpack config to tests

--- test/default.test.js
+++ test/default.test.js
@@ -6,7 +6,7 @@ import WebappWebpackPlugin from '..';
 import {logo, generate, compare, expected} from './util';
 
 test('should generate the expected default result', async t => {
-  const stats = await generate([new WebappWebpackPlugin({logo})]);
+  const stats = await generate({plugins: [new WebappWebpackPlugin({logo})]});
 
   t.context.dist = stats.compilation.compiler.outputPath;
   const diff = await compare(t.context.dist, path.resolve(expected, 'default'));
--- test/html.test.js
+++ test/html.test.js
@@ -7,10 +7,12 @@ import WebappWebpackPlugin from '..';
 import {logo, generate, compare, expected} from './util';
 
 test('should work together with the html-webpack-plugin', async t => {
-  const stats = await generate([
-    new HtmlWebpackPlugin(),
-    new WebappWebpackPlugin({logo}),
-  ]);
+  const stats = await generate({
+    plugins: [
+      new HtmlWebpackPlugin(),
+      new WebappWebpackPlugin({logo}),
+    ],
+  });
 
   t.context.dist = stats.compilation.compiler.outputPath;
   const diff = await compare(t.context.dist, path.resolve(expected, 'html'));
