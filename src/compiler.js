a2edd39eb7976f8d8575add4cca56691f95cd3e1
Bruno Dutra <brunocodutra@gmail.com>
2018-03-10 15:33:21 +0100

    src/favicons.js => src/loader.js

--- src/compiler.js
+++ src/compiler.js
@@ -13,7 +13,7 @@ module.exports.run = ({prefix, favicons, logo}, context, compilation) => {
   const compiler = compilation.createChildCompiler('webapp-webpack-plugin', {filename, publicPath});
   compiler.context = context;
 
-  const loader = require.resolve('./favicons');
+  const loader = require.resolve('./loader');
   const query = JSON.stringify({prefix, options: favicons});
 
   new SingleEntryPlugin(context, `!!${loader}?${query}!${logo}`).apply(compiler);
